db05233df75a2ed6d7adb021eea481a3f7b651e3
= <bkdas@teamdrg.com>
2016-10-12 00:22:40 +0530

    re-organise menu- as we don't need so many levels of menu hierarchy.

--- models/PostCategory.js
+++ models/PostCategory.js
@@ -11,9 +11,11 @@ var PostCategory = new keystone.List('PostCategory', {
 
 PostCategory.add({
 	name: { type: String, required: true },
-	type: { type: Types.Select, options: 'tutorial, blog' },
-	section: { type: Types.Select, options: 'Languages, Frameworks, Technology, Database, Servers, Entrepreneurship, Sales, Finance, Data Science, Internet Of Things' },
-	image_url: { type: String, required: true, default: 'images/' },
+	type: { type: Types.Select, options: 'Tutorial, Blog, Application' },
+	category: { type: Types.Select, options: 'Technology, Business, Academic, Pro-Skill, Competitive Exam, Misc' },
+	section: { type: Types.Select, options: 'Languages, Frameworks, Technology, Database, Servers, Entrepreneurship, Sales, Finance, Data Science, Internet Of Things, Business, UPSC' },
+	subject: { type: Types.Select, options: 'C, C++, AWS, MySQL' },
+	image_url: { type: String, required: true, default: 'images/courses/techdealer.jpg' },
 	tags: { type: Types.TextArray },
 });
 
